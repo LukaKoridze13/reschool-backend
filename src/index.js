e92980e8935ea548a0875113de65376716e11ff6
Luka Koridze <lukakoridze13@gmail.com>
2025-05-17 14:45:37 +0400

    Feat: Swagger API Docs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import mainRouter from "./routes/main.router.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../docs/swagger.json" with { type: "json" };
 
 const app = express();
 
 const corsOptions = {
-  origin: "*", 
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
@@ -17,6 +19,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.set("trust proxy", true);
 app.use("/api", mainRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose
   .connect(process.env.MONGODB_URI)
